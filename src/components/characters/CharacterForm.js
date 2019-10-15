e616e8169327fc29b00a8d790e3730da646e90eb
Michael Hinte <mhinte91@gmail.com>
2019-10-14 23:44:18 -0500

    updates stats in state on click

--- src/components/characters/CharacterForm.js
+++ src/components/characters/CharacterForm.js
@@ -9,7 +9,23 @@ class CharacterForm extends React.Component {
       charClass: '',
       bio: '',
       user: this.props.user ? this.props.user._id : null
-    }
+    },
+    stats: [
+      { stat: 'Strength', value: 0, i: 0 },
+      { stat: 'Dexterity', value: 0, i: 1 },
+      { stat: 'Constitution', value: 0, i: 2 },
+      { stat: 'Intelligence', value: 0, i: 3 },
+      { stat: 'Wisdom', value: 0, i: 4 },
+      { stat: 'Charisma', value: 0, i: 5 }
+    ]
+    // stats: {
+    //   strength: 0,
+    //   dexterity: 0,
+    //   constitution: 0,
+    //   intelligence: 0,
+    //   wisdom: 0,
+    //   charisma: 0
+    // }
   };
 
   formRef = React.createRef();
@@ -36,6 +52,19 @@ class CharacterForm extends React.Component {
     this.setState({
       formData,
       invalidForm: !this.formRef.current.checkValidity()
+    });
+  };
+
+  handleRoll = e => {
+    var stat = this.state.stats.filter(function(item) {
+      return item.stat === e.target.innerHTML;
+    });
+    console.log(stat[0].i);
+    let newStats = JSON.parse(JSON.stringify(this.state.stats));
+    console.log(newStats);
+    newStats[stat[0].i].value = 1;
+    this.setState({
+      stats: newStats
     });
   };
 
@@ -99,6 +128,18 @@ class CharacterForm extends React.Component {
           value={this.state.formData.bio}
           onChange={this.handleChange}
         />
+        <div>
+          <ul className='table th'>
+            {this.state.stats.map((stat, idx) => (
+              <div key={idx}>
+                <div className='btn badge' onClick={this.handleRoll}>
+                  {stat.stat}
+                </div>
+                <div className='btn badge'>{stat.value}</div>
+              </div>
+            ))}
+          </ul>
+        </div>
         <div>
           <input
             type='submit'
