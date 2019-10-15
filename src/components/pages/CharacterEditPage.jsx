907dad8ecce1a1e1b9fee61bed1bf3e8874226b7
Michael Hinte <mhinte91@gmail.com>
2019-10-14 21:55:47 -0500

    fixes update screen

--- src/components/pages/CharacterEditPage.jsx
+++ src/components/pages/CharacterEditPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as characterAPI from '../../services/characters-api';
 
 class CharacterEditPage extends React.Component {
@@ -32,68 +33,77 @@ class CharacterEditPage extends React.Component {
 
   render() {
     return (
-      <form ref={this.formRef} onSubmit={this.handleSubmit}>
-        <h2 className='text-primary'>Add Character</h2>
-        <input
-          type='text'
-          placeholder='Name'
-          name='name'
-          value={this.state.formData.name}
-          onChange={this.handleChange}
-          required
-        />
-        <select
-          type='text'
-          placeholder='Race'
-          name='race'
-          value={this.state.formData.race}
-          onChange={this.handleChange}
-        >
-          <option hidden>--Select Race--</option>
-          <option value='Dragonborn'>Dragonborn</option>
-          <option value='Dwarf'>Dwarf</option>
-          <option value='Elf'>Elf</option>
-          <option value='Gnome'>Gnome</option>
-          <option value='Half-Elf'>Half-Elf</option>
-          <option value='Half-Orc'>Half-Orc</option>
-          <option value='Halfling'>Halfling</option>
-          <option value='Human'>Human</option>
-          <option value='Tiefling'>Tiefling</option>
-        </select>
-        <br />
-        <select
-          type='text'
-          placeholder='Class'
-          name='charClass'
-          value={this.state.formData.charClass}
-          onChange={this.handleChange}
-        >
-          <option hidden>--Select Class--</option>
-          <option value='Barbarian'>Barbarian</option>
-          <option value='Bard'>Bard</option>
-          <option value='Cleric'>Cleric</option>
-          <option value='Druid'>Druid</option>
-          <option value='Fighter'>Fighter</option>
-          <option value='Monk'>Monk</option>
-          <option value='Paladin'>Paladin</option>
-          <option value='Ranger'>Ranger</option>
-        </select>
-        <br />
-        <textarea
-          type='text'
-          placeholder='Bio'
-          name='bio'
-          value={this.state.formData.bio}
-          onChange={this.handleChange}
-        />
-        <div>
+      <div className='grid-3'>
+        <div></div>
+        <form ref={this.formRef} onSubmit={this.handleSubmit}>
+          <h2 className='text-primary'>
+            Edit {this.props.location.state.character.name}
+          </h2>
           <input
-            type='submit'
-            value='Update Character'
-            className='btn btn-primary btn-block'
+            type='text'
+            placeholder='Name'
+            name='name'
+            value={this.state.formData.name}
+            onChange={this.handleChange}
+            required
           />
-        </div>
-      </form>
+          <select
+            type='text'
+            placeholder='Race'
+            name='race'
+            value={this.state.formData.race}
+            onChange={this.handleChange}
+          >
+            <option hidden>--Select Race--</option>
+            <option value='Dragonborn'>Dragonborn</option>
+            <option value='Dwarf'>Dwarf</option>
+            <option value='Elf'>Elf</option>
+            <option value='Gnome'>Gnome</option>
+            <option value='Half-Elf'>Half-Elf</option>
+            <option value='Half-Orc'>Half-Orc</option>
+            <option value='Halfling'>Halfling</option>
+            <option value='Human'>Human</option>
+            <option value='Tiefling'>Tiefling</option>
+          </select>
+          <br />
+          <select
+            type='text'
+            placeholder='Class'
+            name='charClass'
+            value={this.state.formData.charClass}
+            onChange={this.handleChange}
+          >
+            <option hidden>--Select Class--</option>
+            <option value='Barbarian'>Barbarian</option>
+            <option value='Bard'>Bard</option>
+            <option value='Cleric'>Cleric</option>
+            <option value='Druid'>Druid</option>
+            <option value='Fighter'>Fighter</option>
+            <option value='Monk'>Monk</option>
+            <option value='Paladin'>Paladin</option>
+            <option value='Ranger'>Ranger</option>
+          </select>
+          <br />
+          <textarea
+            type='text'
+            placeholder='Bio'
+            name='bio'
+            value={this.state.formData.bio}
+            onChange={this.handleChange}
+          />
+          <div>
+            <input
+              type='submit'
+              value='Update Character'
+              className='btn btn-primary btn-block'
+            />
+          </div>
+          <div className='btn btn-block text-primary text-center '>
+            <Link to='/'>Cancel</Link>
+          </div>
+        </form>
+        <div></div>
+      </div>
     );
   }
 }
